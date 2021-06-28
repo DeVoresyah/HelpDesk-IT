9438c0da4e9da6f76cfcf3a196c94d7a978e04aa
DeVoresyah ArEst <de.voresyah@gmail.com>
2021-06-29 00:02:11 +0700

    feat: init register screen

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -6,12 +6,14 @@ const Stack = createStackNavigator()
 
 // Auth
 import LoginScreen from "../Containers/Auth/LoginScreen"
+import RegisterScreen from "../Containers/Auth/RegisterScreen"
 
 const AppNavigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
